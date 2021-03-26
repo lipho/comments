bb62137e746457daa5cd5eaabea163b8d6e64f34
lipho <hello@lipho.net>
2021-03-26 16:55:46 -0700

    Add ApprovalCard component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CommentDetail from "./CommentDetail";
 import {image} from "faker";
+
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => (
   <div className='ui container comments'>
-    <CommentDetail comment='Nice going' avatar={image.avatar()} author='Sam' timeAgo='Today at 3:15PM'/>
-    <CommentDetail comment='gg wp' avatar={image.avatar()} author='Harley' timeAgo='Today at 2:30PM'/>
-    <CommentDetail comment='awesome blog!!!' avatar={image.avatar()} author='King Shark' timeAgo='Today at 4:45PM'/>
+    <ApprovalCard>
+      <CommentDetail comment='gg wp' avatar={image.avatar()} author='Harley' timeAgo='Today at 2:30PM'/>
+    </ApprovalCard>
+    <ApprovalCard>
+      <CommentDetail comment='awesome blog!!!' avatar={image.avatar()} author='King Shark' timeAgo='Today at 4:45PM'/>
+    </ApprovalCard>
+    <ApprovalCard>
+      <CommentDetail comment='Nice going' avatar={image.avatar()} author='Sam' timeAgo='Today at 3:15PM'/>
+    </ApprovalCard>
   </div>
 );
 
